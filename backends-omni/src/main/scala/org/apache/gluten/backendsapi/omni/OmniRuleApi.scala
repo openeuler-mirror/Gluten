9e9a8cf0945ba30a4e13d3c49f17bd7fa5b48c47
shalili <s30074194@china.huawei.com>
2025-05-20 12:47:47 +0800

    change RewriteAQEShuffleRead import path

--- backends-omni/src/main/scala/org/apache/gluten/backendsapi/omni/OmniRuleApi.scala
+++ backends-omni/src/main/scala/org/apache/gluten/backendsapi/omni/OmniRuleApi.scala
@@ -28,7 +28,7 @@ import org.apache.gluten.extension.columnar.transition.{InsertTransitions, Remov
 import org.apache.gluten.extension.columnar.validator.{Validator, Validators}
 import org.apache.gluten.extension.injector.{Injector, SparkInjector}
 import org.apache.gluten.extension.injector.GlutenInjector.{LegacyInjector, RasInjector}
-import org.apache.gluten.extension.MiscColumnarRules.RewriteAQEShuffleRead
+import org.apache.gluten.extension.RewriteAQEShuffleRead
 import org.apache.spark.sql.execution.{ColumnarCollapseTransformStages, GlutenFallbackReporter}
 
 class OmniRuleApi extends RuleApi {
