28d79618a9185f127ddeb5ef4511fd63cc9d27c5
无需 <12625183+wuxu_2023@user.noreply.gitee.com>
2025-06-18 13:10:20 +0000

    !87 fix make_decimal bug * update cpp-omni/src/substrait/SubstraitParser.cpp.

--- cpp-omni/src/substrait/SubstraitParser.cpp
+++ cpp-omni/src/substrait/SubstraitParser.cpp
@@ -355,7 +355,7 @@ SubstraitParser::substraitOmniFunctionMap = {
     {"max", {FUNCTION_OMNI_EXPR_TYPE, "max"}},
     {"avg", {FUNCTION_OMNI_EXPR_TYPE, "avg"}},
     {"first", {FUNCTION_OMNI_EXPR_TYPE, "first"}},
-    {"make_decimal", {FUNCTION_OMNI_EXPR_TYPE, "make_decimal"}},
+    {"make_decimal", {FUNCTION_OMNI_EXPR_TYPE, "MakeDecimal"}},
     {"unix_timestamp", {FUNCTION_OMNI_EXPR_TYPE, "unix_timestamp"}},
     {"from_unixtime", {FUNCTION_OMNI_EXPR_TYPE, "from_unixtime"}},
     {"first_ignore_null", {FUNCTION_OMNI_EXPR_TYPE, "first_ignore_null"}},
