6daf94ad10f622f27c9b51b6e0c78e15b4a8beb1
helloxteen <helloxteen@foxmail.com>
2026-07-02 19:26:53 +0800

    support UTC for omni unix time functions

--- backends-omni/src/main/scala/org/apache/gluten/expression/OmniExpressionAdaptor.scala
+++ backends-omni/src/main/scala/org/apache/gluten/expression/OmniExpressionAdaptor.scala
@@ -82,7 +82,7 @@ object OmniExpressionAdaptor extends Logging {
     "yyyy-MM", "yyyy-MM-dd", "yyyy-MM-dd HH", "yyyy-MM-dd HH:mm", "yyyy-MM-dd HH:mm:ss",
     "yyyyMM", "yyyyMMdd", "yyyyMMddHH", "yyyyMMddHHmm", "yyyyMMddHHmmss"
   )
-  private val timeZoneSet: Set[String] = Set("GMT+08:00", "Asia/Beijing", "Asia/Shanghai")
+  private val timeZoneSet: Set[String] = Set("GMT+08:00", "Asia/Beijing", "Asia/Shanghai", "UTC")
 
   private def unsupportedUnixTimeFunction(timeFormat: String, timeZone: String): Unit = {
     if (!GlutenConfig.get.enableOmniUnixTimeFunc) {
